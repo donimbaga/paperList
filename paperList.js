40e86eb56400cdd18d7689e16d7c9d4d0e83ba9b
donimbaga <donibrysonimbaga@gmail.com>
2020-08-12 01:39:37 +0300

    Add lists

--- paperList.js
+++ paperList.js
@@ -1,12 +1,23 @@
 /* Doni Brysoni Mbaga */
 
 
-setInterval(today, 500);
+setInterval(showToday, 500);
 
-function today() {
+function showToday() {
   const date = new Date().toDateString();
   const time = new Date().toLocaleTimeString();
 
   document.getElementById("today").innerHTML = date + ", " + time;
 }
+
+const addButt = document.getElementById("addButt");
+addButt.addEventListener("click", addListItem);
+
+function addListItem() {
+  const listItem = document.createElement("input");
+  listItem.setAttribute("class", "listItem");
+  listItem.setAttribute("id", "list");
+  const main = document.getElementsByTagName("main")[0];
+  main.appendChild(listItem);
+} 
 
