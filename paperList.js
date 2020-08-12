f5482df3ba0034807a715bd301046e9ed2253fe2
donimbaga <donibrysonimbaga@gmail.com>
2020-08-12 03:15:07 +0300

    Rhumba....

--- paperList.js
+++ paperList.js
@@ -15,9 +15,17 @@ addButt.addEventListener("click", addListItem);
 
 function addListItem() {
   const listItem = document.createElement("input");
+  listItem.setAttribute("type", "text");
   listItem.setAttribute("class", "listItem");
   listItem.setAttribute("id", "list");
   const main = document.getElementsByTagName("main")[0];
   main.appendChild(listItem);
+
+  const input = document.querySelector("input:last-child");
+  input.addEventListener("keyup", (event) => {
+    if (event.keyCode === 13) {
+       console.log("You pressed 'enter'");
+    } 
+  });
 } 
 
